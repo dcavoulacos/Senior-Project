b7b77328364852e9241b349556cc282eb278e42c
vagrant <vagrant@precise32.(none)>
2014-02-22 21:38:39 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeniorProject::Application.config.secret_token = 'bbd01e96462fdfa8921cc2d660f4f494f89d51ce8e3a6e726aeef98b008b8f07b4bb290048a5b4e62093cc19a14ddfc6e1b8cb496c2b3f16efd1c135a9b9d2a9'
